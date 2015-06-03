de69c7153fb7ed2d0d55964acbf6a22df478c2ac
pmadrigal <pedromadri89@hotmail.com>
2015-06-03 15:39:04 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,36 +2,52 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import javax.net.ssl.SSLSocketFactory;
 
 /**
- * Created by pmadrigal on 8/05/15.
+ * Created by pmadrigal on 03/06/15.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
 
         /**
-        * Generate a Java™ key store (JKS) file using the JDK keytool program
-        * keytool -importcert -file /PATH/TO/mongodb-ca.crt -storepass PASSWORD -alias server.mongodb.com -keystore /PATH/TO/mongodb.keystore
-        * */
-        System.setProperty("javax.net.ssl.keyStore", "/etc/ssl/mongodb.keystore");
-        System.setProperty("javax.net.ssl.keyStorePassword", "password");
-        System.setProperty("javax.net.ssl.trustStore", "/etc/ssl/mongodb.keystore");
-        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+         * Generate a Java™ key store (JKS) file using the JDK keytool program
+         * keytool -importcert -file /PATH/TO/mongodb-ca.crt -storepass PASSWORD -alias server.mongodb.com -keystore /PATH/TO/mongodb.keystore
+         * */
 
-        ServerAddress hostPort = new ServerAddress("localhost", 27017);
+        /**
+         System.setProperty("javax.net.ssl.keyStore", "/etc/ssl/mongodb.keystore");
+         System.setProperty("javax.net.ssl.keyStorePassword", "password");
+         System.setProperty("javax.net.ssl.trustStore", "/etc/ssl/mongodb.keystore");
+         System.setProperty("javax.net.ssl.trustStorePassword", "password");
+         */
 
-        MongoClientOptions options = new MongoClientOptions.Builder().socketFactory(SSLSocketFactory.getDefault()).build();
-        MongoClient mongoClient = new MongoClient( hostPort, options );
+        Integer i = 0;
+        for (i =0; i<1000; i++) {
+            ServerAddress hostPort = new ServerAddress("conectores02", 27017);
 
+            // MongoClientOptions options = new MongoClientOptions.Builder().socketFactory(SSLSocketFactory.getDefault()).build();
+            MongoClient mongoClient = new MongoClient(hostPort);
 
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        MongoCollection<Document> collection = database.getCollection("mycollection");
-        System.out.println("Número de collecciones "+collection.count());
+            MongoDatabase database = mongoClient.getDatabase("local");
+            MongoCollection<Document> collection = database.getCollection("slaves");
+            System.out.println("Número de elementos en la collección: " + collection.count());
+
+            MongoCursor<Document> cursor = collection.find().iterator();
+            try {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            } finally {
+                cursor.close();
+            }
+
+            mongoClient.close();
+
+        }
     }
 }
